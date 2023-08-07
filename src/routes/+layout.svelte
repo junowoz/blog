53898627013d4fc182add22ce6cfbe7d2198cbf4
junowoz <junogouvea@gmail.com>
2023-08-07 14:41:10 -0400

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 	import Footer from '../components/Footer.svelte';
 </script>
 
-<div class="max-w-screen-sm sm:w-full mx-4 md:mx-auto my-2">
+<main class="max-w-screen-sm sm:w-full mx-4 md:mx-auto my-2">
 	<slot />
 	<Footer />
-</div>
+</main>
