45c41f2446ad854c569857d1294b8692f3aeb2dc
junowoz <junogouvea@gmail.com>
2023-08-08 01:41:10 -0400

    google analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,23 @@
 	import Footer from '../components/Footer.svelte';
 	import '../styles/app.css';
 	import '../styles/tailwind.css';
+
+	if(typeof window !== 'undefined' && window) {
+		const googleAnalyticsId = 'G-RVMBTKCTP0';
+		// @ts-ignore
+		window.dataLayer = window.dataLayer || [];
+		// @ts-ignore
+		function gtag(){window.dataLayer.push(arguments);}
+		gtag('js', new Date());
+		gtag('config', googleAnalyticsId);
+	}
 </script>
 
-<body class="max-w-screen-sm sm:w-full mx-2 md:mx-auto my-2 bg-inherit dark:bg-neutral-800">
+<svelte:head>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-RVMBTKCTP0"></script>
+</svelte:head>
+
+<main class="max-w-screen-sm sm:w-full mx-2 md:mx-auto my-2 bg-inherit dark:bg-neutral-800">
 	<slot />
 	<Footer />
-</body>
+</main>
